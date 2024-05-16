286f8d02e4c006fdf2eb617c600912047ae6b4c6
xuezhao <zxue@redhat.com>
2024-05-16 09:56:57 +0800

    Fix: add a condition of manifestapplied for hosted addon. (#267)
    Signed-off-by: xuezhaojun <zxue@redhat.com>

--- pkg/addonmanager/controllers/agentdeploy/controller.go
+++ pkg/addonmanager/controllers/agentdeploy/controller.go
@@ -392,6 +392,12 @@ func (c *addonDeployController) buildDeployManifestWorksFunc(addonWorkBuilder *a
 			return nil, nil, err
 		}
 		if len(objects) == 0 {
+			meta.SetStatusCondition(&addon.Status.Conditions, metav1.Condition{
+				Type:    appliedType,
+				Status:  metav1.ConditionTrue,
+				Reason:  addonapiv1alpha1.AddonManifestAppliedReasonManifestsApplied,
+				Message: "no manifest need to apply",
+			})
 			return nil, nil, nil
 		}
 
