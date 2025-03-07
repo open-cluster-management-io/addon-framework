cf8513521c0aa39647bc43c4482c3bc9bfb6ac04
Jian Qiu <jqiu@redhat.com>
2025-03-07 15:51:20 +0800

    Bump ocm api and sdk-go to v0.16.0
    Signed-off-by: Jian Qiu <jqiu@redhat.com>

--- test/e2e/helloworld_test.go
+++ test/e2e/helloworld_test.go
@@ -103,20 +103,6 @@ var _ = ginkgo.Describe("install/uninstall helloworld addons", func() {
 	})
 
 	ginkgo.It("addon should be worked", func() {
-		ginkgo.By("Make sure cma annotation managed by addon-manager is added")
-		gomega.Eventually(func() error {
-			cma, err := hubAddOnClient.AddonV1alpha1().ClusterManagementAddOns().Get(context.Background(), addonName, metav1.GetOptions{})
-			if err != nil {
-				return err
-			}
-
-			if cma.Annotations[addonapiv1alpha1.AddonLifecycleAnnotationKey] != addonapiv1alpha1.AddonLifecycleAddonManagerAnnotationValue {
-				return fmt.Errorf("addon should have annotation, but get %v", cma.Annotations)
-			}
-
-			return nil
-		}, eventuallyTimeout, eventuallyInterval).ShouldNot(gomega.HaveOccurred())
-
 		ginkgo.By("Make sure addon is available")
 		gomega.Eventually(func() error {
 			addon, err := hubAddOnClient.AddonV1alpha1().ManagedClusterAddOns(managedClusterName).Get(context.Background(), addonName, metav1.GetOptions{})
